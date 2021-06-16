854b6cd498c3c8030ebb4e69ebbca3dc8aab6fb4
ArthurSudbrackIbarra <arthur.ibarra@edu.pucrs.br>
2021-06-16 15:11:40 -0300

    Terminando fill de possibleMovements.

--- src/Board.java
+++ src/Board.java
@@ -18,6 +18,10 @@ public class Board {
             possibleMovements = new ArrayList<>();
         }
 
+        public void addPossibleMovement(Block block){
+            this.possibleMovements.add(block);
+        }
+
     }
 
     private Block[][] board;
@@ -41,7 +45,7 @@ public class Board {
         reader.close();
 
         // Creating the board.
-        this.board = new Block[columnCount][lineCount];
+        this.board = new Block[lineCount][columnCount];
 
         // Filling the board.
         reader = Files.newBufferedReader(path, Charset.defaultCharset());
@@ -49,9 +53,75 @@ public class Board {
         while((line = reader.readLine()) != null){
             if(line.equals("")) continue;
             for(int i = 0; i < line.length(); i++){
-                this.board[i][j] = new Block(line.charAt(i));
+                char symbol = line.charAt(i);
+                Block block = new Block(symbol);
+                this.board[j][i] = block;
+                if(symbol == 'C'){
+                    this.startBlock = block;
+                }
+                else if(symbol == 'S'){
+                    this.endBlock = block;
+                }
             }
             j++;
+        }
+        fillPossibleMovements();
+    }
+
+    public void fillPossibleMovements(){
+        int lines = this.board.length;
+        int columns = this.board[0].length;
+        for(int i = 0; i < lines; i++){
+            for(int j = 0; j < columns; j++){
+                Block block = this.board[i][j];
+
+                // Block above.
+                int above = i - 1;
+                if(above < 0) above  = lines - 1;
+                block.addPossibleMovement(this.board[above][j]);
+
+                // Block below.
+                int below = i + 1;
+                if(below >= lines) below = 0;
+                block.addPossibleMovement(this.board[below][j]);
+
+                // Block to the left.
+                int left = j - 1;
+                if(left < 0) left = columns - 1;
+                block.addPossibleMovement(this.board[i][left]);
+
+                // Block to the right.
+                int right = j + 1;
+                if(right >= columns) right = 0;
+                block.addPossibleMovement(this.board[i][right]);
+
+                // Block to the diagonal up left.
+                int diagonalUpLeftX = i - 2;
+
+                if(diagonalUpLeftX == -2){
+                    diagonalUpLeftX = (lines - 1) - 1;
+                }
+                else if(diagonalUpLeftX == -1){
+                    diagonalUpLeftX = lines - 1;
+                }
+
+                int diagonalUpLeftY = j - 2;
+
+                if(diagonalUpLeftY == -2){
+                    diagonalUpLeftY = (columns - 1) - 1;
+                }
+                else if(diagonalUpLeftY == -1){
+                    diagonalUpLeftY = columns - 1;
+                }
+                block.addPossibleMovement(this.board[diagonalUpLeftX][diagonalUpLeftY]);
+
+                // Block to the diagonal up right.
+
+
+                int diagonalUpRight;
+                int diagonalDownLeft;
+                int diagonalDownRight;
+            }
         }
     }
 
