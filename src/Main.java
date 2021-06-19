ce9c8ca08dcce96b5f6618f64bff2fa9979994c6
ArthurSudbrackIbarra <arthur.ibarra@edu.pucrs.br>
2021-06-19 15:45:23 -0300

    Mensagens de caminho atualizadas.

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         try{
-            Labyrinth labyrinth = new Labyrinth("caso200.txt");
-
-            System.out.print("[Labirinto]\n");
-            labyrinth.printLabyrinth();
+            Labyrinth labyrinth = new Labyrinth("caso150.txt");
 
             int numberOfSteps = labyrinth.findShortestPath();
             if(numberOfSteps == 0){
-                System.out.print("\nNao ha caminho para o labirinto em questao.");
+                System.out.print("Nao ha caminho para o labirinto em questao.");
             }
-
-            System.out.print("\n[Menor caminho]\n");
-            labyrinth.printLabyrinth();
-            System.out.print("\nNumero de passos: " + numberOfSteps);
-            System.out.print("\n\nAs bolinhas 'O' indicam por onde o carneirinho passou.");
+            else{
+                System.out.print("\n[Menor caminho]\n");
+                labyrinth.printLabyrinth();
+                System.out.print("\nNumero de passos: " + numberOfSteps);
+                System.out.print("\n\nAs bolinhas 'O' indicam por onde o carneirinho passou.");
+            }
         }
         catch (IOException error){
             System.out.println("Erro!");
         }
     }
-
 }
